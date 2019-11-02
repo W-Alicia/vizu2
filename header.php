5deee4ef3009e073f76a1e3189ee244c115de4a2
Kanny Gusenga <kgml94@hotmail.com>
2019-11-02 19:51:46 +0100

    page création

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-<title>etudiant</title>
+<title>Vizu2</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <link href="https://use.fontawesome.com/releases/v5.0.1/css/all.css" rel="stylesheet">
@@ -19,9 +19,9 @@
       <div class="overlay" id="overlay">
         <nav class="overlay-menu">
           <ul>
-            <li ><a href="#">Vizu-L</a></li>
+            <li ><a href="archive-creation.php">Vizu-L</a></li>
             <li><a href="#">Vizuthèque</a></li>
-            <li><a href="#">Portfolios</a></li>
+            <li><a href="archive-etudiant.php">Portfolios</a></li>
             <li><a href="#">Filières</a></li>
             <li><a href="#">Events</a></li>
           </ul>
