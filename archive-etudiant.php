ba696e1389447ddc8e1c44481e50c69a8ff8de78
Alicia Wiot <xsabaku@gmail.com>
2019-11-03 21:02:34 +0100

    creation

---
+++ archive-etudiant.php
@@ -0,0 +1,100 @@
+<?php require("header.php") ?>
+
+<div class="container etudiants">
+        <div class="row justify-content-center recherche">
+                <div class="col-6 filtre">
+                        <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search">
+                                <button id="search" class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                        </form> <!--form search-->
+                </div>
+                <div class="col filtre cat">
+                    a-z
+                </div>
+                <div class="col filtre cat">
+                    categorie
+                </div>
+                <div class="col filtre cat">
+                    cycle
+                </div>
+        </div><!--row recherche-->
+
+        <div class="row justify-content-center etudiants">
+            <div  class="col-3 etu">
+                <div class="etu-picto">
+                  <img src="img/picto-logo.svg" alt="pictogramme">
+                </div>
+                <div class="etu-info">
+                    <h5>nom</h5>
+                    <h6>categorie</h6>
+                </div>
+            </div>
+            <div  class="col-3 etu">
+                <div class="etu-picto">
+                  <img src="img/picto-logo.svg" alt="pictogramme">
+                </div>
+                <div class="etu-info">
+                    <h5>nom</h5>
+                    <h6>categorie</h6>
+                </div>
+            </div>
+            <div class="col-3 etu">
+                <div class="etu-picto">
+                  <img src="img/picto-logo.svg" alt="pictogramme">
+                </div>
+                <div class="etu-info">
+                    <h5>nom</h5>
+                    <h6>categorie</h6>
+                </div>
+              </div>
+              <div  class="col-3 etu">
+                <div class="etu-picto">
+                  <img src="img/picto-logo.svg" alt="pictogramme">
+                </div>
+                <div class="etu-info">
+                    <h5>nom</h5>
+                    <h6>categorie</h6>
+                </div>
+              </div>
+
+              <div  class="col-3 etu">
+                <div class="etu-picto">
+                  <img src="img/picto-logo.svg" alt="pictogramme">
+                </div>
+                <div class="etu-info">
+                    <h5>nom</h5>
+                    <h6>categorie</h6>
+                </div>
+              </div>
+              <div  class="col-3 etu">
+                <div class="etu-picto">
+                  <img src="img/picto-logo.svg" alt="pictogramme">
+                </div>
+                <div class="etu-info">
+                    <h5>nom</h5>
+                    <h6>categorie</h6>
+                </div>
+              </div>
+              <div  class="col-3 etu">
+                <div class="etu-picto">
+                  <img src="img/picto-logo.svg" alt="pictogramme">
+                </div>
+                <div class="etu-info">
+                    <h5>nom</h5>
+                    <h6>categorie</h6>
+                </div>
+              </div>
+              <div  class="col-3 etu">
+                <div class="etu-picto">
+                  <img src="img/picto-logo.svg" alt="pictogramme">
+                </div>
+                <div class="etu-info">
+                    <h5>nom</h5>
+                    <h6>categorie</h6>
+                </div>
+              </div>
+        </div> <!--row etu-->
+</div> <!--container etudiants-->
+
+
+<?php require("footer.php") ?>
