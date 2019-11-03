567bb13ced49439a8775357f1dba41659a7ef91a
Alicia Wiot <xsabaku@gmail.com>
2019-11-03 21:12:38 +0100

    creation

--- archive-creation.php
+++ archive-creation.php
@@ -1,13 +1,10 @@
 <?php require("header.php") ?>
-
+<div class="container gauche">
+<img src="img/creation.svg" alt="Avatar" class="imagecrea" width="1100px">
+</div>
 <div class="container etudiants">
         <div class="row justify-content-center recherche">
-                <div class="col-6 filtre">
-                        <form class="form-inline my-2 my-lg-0">
-                                <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search">
-                                <button id="search" class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
-                        </form> <!--form search-->
-                </div>
+
                 <div class="col filtre cat">
                     a-z
                 </div>
@@ -21,80 +18,117 @@
 
         <div class="row justify-content-center etudiants">
             <div  class="col-3 etu">
-                <div class="etu-picto">
-                  <img src="img/picto-logo.svg" alt="pictogramme">
-                </div>
-                <div class="etu-info">
-                    <h5>nom</h5>
-                    <h6>categorie</h6>
-                </div>
-            </div>
-            <div  class="col-3 etu">
-                <div class="etu-picto">
-                  <img src="img/picto-logo.svg" alt="pictogramme">
-                </div>
-                <div class="etu-info">
-                    <h5>nom</h5>
-                    <h6>categorie</h6>
-                </div>
-            </div>
-            <div class="col-3 etu">
-                <div class="etu-picto">
-                  <img src="img/picto-logo.svg" alt="pictogramme">
-                </div>
-                <div class="etu-info">
-                    <h5>nom</h5>
-                    <h6>categorie</h6>
-                </div>
-              </div>
-              <div  class="col-3 etu">
-                <div class="etu-picto">
-                  <img src="img/picto-logo.svg" alt="pictogramme">
-                </div>
-                <div class="etu-info">
-                    <h5>nom</h5>
-                    <h6>categorie</h6>
-                </div>
-              </div>
+               
+                <img src="img/un.jpg" alt="Avatar" class="image">
+                  <div class="overlay2"> <img src="img/un.jpg" alt="Avatar" class="image">
+                    <div class="text">Bla bla bla</div>
+  </div>
+       </div>
 
-              <div  class="col-3 etu">
-                <div class="etu-picto">
-                  <img src="img/picto-logo.svg" alt="pictogramme">
-                </div>
-                <div class="etu-info">
-                    <h5>nom</h5>
-                    <h6>categorie</h6>
-                </div>
-              </div>
-              <div  class="col-3 etu">
-                <div class="etu-picto">
-                  <img src="img/picto-logo.svg" alt="pictogramme">
-                </div>
-                <div class="etu-info">
-                    <h5>nom</h5>
-                    <h6>categorie</h6>
-                </div>
-              </div>
-              <div  class="col-3 etu">
-                <div class="etu-picto">
-                  <img src="img/picto-logo.svg" alt="pictogramme">
-                </div>
-                <div class="etu-info">
-                    <h5>nom</h5>
-                    <h6>categorie</h6>
-                </div>
-              </div>
-              <div  class="col-3 etu">
-                <div class="etu-picto">
-                  <img src="img/picto-logo.svg" alt="pictogramme">
-                </div>
-                <div class="etu-info">
-                    <h5>nom</h5>
-                    <h6>categorie</h6>
-                </div>
-              </div>
+
+
+            <div  class="col-3 etu">
+             
+                <img src="img/deux.jpg" alt="Avatar" class="image">
+                  <div class="overlay2"> <img src="img/deux.jpg" alt="Avatar" class="image">
+                     <div class="text">Hello World</div>
+  </div>
+      </div>
+
+
+      <div  class="col-3 etu">
+             
+             <img src="img/trois.jpg" alt="Avatar" class="image">
+               <div class="overlay2"> <img src="img/trois.jpg" alt="Avatar" class="image">
+                  <div class="text">Hello World</div>
+</div>
+   </div>
+
+
+   <div  class="col-3 etu">
+             
+             <img src="img/quatre.jpg" alt="Avatar" class="image">
+               <div class="overlay2"> <img src="img/quatre.jpg" alt="Avatar" class="image">
+                  <div class="text">Hello World</div>
+</div>
+   </div>
+
+   <div  class="col-3 etu">
+             
+             <img src="img/cinq.jpg" alt="Avatar" class="image">
+               <div class="overlay2"> <img src="img/cinq.jpg" alt="Avatar" class="image">
+                  <div class="text">Hello World</div>
+</div>
+   </div>
+
+   <div  class="col-3 etu">
+             
+             <img src="img/six.jpg" alt="Avatar" class="image">
+               <div class="overlay2"> <img src="img/six.jpg" alt="Avatar" class="image">
+                  <div class="text">Hello World</div>
+</div>
+   </div>
+
+   <div  class="col-3 etu">
+             
+             <img src="img/sept.jpg" alt="Avatar" class="image">
+               <div class="overlay2"> <img src="img/sept.jpg" alt="Avatar" class="image">
+                  <div class="text">Hello World</div>
+</div>
+   </div>
+
+   <div  class="col-3 etu">
+             
+             <img src="img/huit.jpg" alt="Avatar" class="image">
+               <div class="overlay2"> <img src="img/huit.jpg" alt="Avatar" class="image">
+                  <div class="text">Hello World</div>
+</div>
+   </div>
+
+   <div  class="col-3 etu">
+             
+             <img src="img/neuf.jpg" alt="Avatar" class="image">
+               <div class="overlay2"> <img src="img/neuf.jpg" alt="Avatar" class="image">
+                  <div class="text">Hello World</div>
+</div>
+   </div>
+
+   <div  class="col-3 etu">
+             
+             <img src="img/dix.jpg" alt="Avatar" class="image">
+               <div class="overlay2"> <img src="img/dix.jpg" alt="Avatar" class="image">
+                  <div class="text">Hello World</div>
+</div>
+   </div>
+
+   <div  class="col-3 etu">
+             
+             <img src="img/onze.jpg" alt="Avatar" class="image">
+               <div class="overlay2"> <img src="img/onze.jpg" alt="Avatar" class="image">
+                  <div class="text">Hello World</div>
+</div>
+   </div>
+
+   <div  class="col-3 etu">
+             
+             <img src="img/douze.jpg" alt="Avatar" class="image">
+               <div class="overlay2"> <img src="img/douze.jpg" alt="Avatar" class="image">
+                  <div class="text">Hello World</div>
+</div>
+   </div>
+
+
+
+
+
+
+
+
         </div> <!--row etu-->
 </div> <!--container etudiants-->
+
+
+
 
 
 <?php require("footer.php") ?>
