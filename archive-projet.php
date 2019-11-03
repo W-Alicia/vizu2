caee65b74bdb97378a4d618ccd0ae0643d1a0f57
Kanny Gusenga <kgml94@hotmail.com>
2019-11-03 19:51:28 +0100

    page projet

--- archive-projet.php
+++ archive-projet.php
@@ -1,6 +1,91 @@
 <?php require("header.php") ?>
 
+<div class="container etudiants">
+        <div class="row justify-content-center recherche">
+                <div class="col-6 filtre">
+                        <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" placeholder="" aria-label="Search">
+                                <button id="search" class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+                        </form> <!--form search-->
+                </div>
+                <div class="col filtre cat">
+                    A-Z
+                </div>
+                <div class="col filtre cat">
+                    Categorie
+                </div>
+                <div class="col filtre cat">
+                    Année
+                </div>
+        </div><!--row recherche-->
 
+        <div class="row etudiants">
+            
+            <div  class="col-6 col-md-4 col-lg-3 etu">
+                <a>
+                    <div class="etu-info">
+                        <h5>Projet</h5>
+                    </div>
+                    <div class="etu-picto">
+                    <img src="img/picto-logo.svg" alt="pictogramme">
+                    </div>
+                </a>    
+            </div>
+            <div  class="col-6 col-md-4 col-lg-3 etu">
+                <a>
+                    <div class="etu-info">
+                        <h5>Projet</h5>
+                    </div>
+                    <div class="etu-picto">
+                    <img src="img/picto-logo.svg" alt="pictogramme">
+                    </div>
+                </a>    
+            </div>
+            <div  class="col-6 col-md-4 col-lg-3 etu">
+                <a>
+                    <div class="etu-info">
+                        <h5>Projet</h5>
+                    </div>
+                    <div class="etu-picto">
+                    <img src="img/picto-logo.svg" alt="pictogramme">
+                    </div>
+                </a>    
+            </div>
+            <div  class="col-6 col-md-4 col-lg-3 etu">
+                <a>
+                    <div class="etu-info">
+                        <h5>Projet</h5>
+                    </div>
+                    <div class="etu-picto">
+                    <img src="img/picto-logo.svg" alt="pictogramme">
+                    </div>
+                </a>    
+            </div>
+            <div  class="col-6 col-md-4 col-lg-3 etu">
+                <a>
+                    <div class="etu-info">
+                        <h5>Projet</h5>
+                    </div>
+                    <div class="etu-picto">
+                    <img src="img/picto-logo.svg" alt="pictogramme">
+                    </div>
+                </a>    
+            </div>
+            <div  class="col-6 col-md-4 col-lg-3 etu">
+                <a>
+                    <div class="etu-info">
+                        <h5>Projet</h5>
+                    </div>
+                    <div class="etu-picto">
+                    <img src="img/picto-logo.svg" alt="pictogramme">
+                    </div>
+                </a>    
+            </div>
+           
+            
+            
+            
+</div>
 
 
 <?php require("footer.php") ?>
