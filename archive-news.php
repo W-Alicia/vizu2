6f8c6a8ba29dd15f710d0ea5a6bd915c99a93e43
Elzbieta Pogrebniak <ela.pogrebniak@gmail.com>
2019-11-11 17:48:51 +0100

    archive news

---
+++ archive-news.php
@@ -0,0 +1,109 @@
+<?php require("header.php") ?>
+
+
+
+<div class="container archive-news">
+  <div class="row text-uppercase">
+    <div class="col-12 col-lg-5 show-news">
+      <div class="show-img">
+          <img src="img/huit.jpg" alt="image">
+          <div class="info-news">
+            <h4>BOZAR</h4>
+            <h5>Fride 03/19</h5>
+            <p>l'expo sur l'expo est une expo tellement expo.<br>l'espo est hostée par ça.</p>
+          </div>
+      </div>
+
+    <div class="row description-event">
+        <div class="col-6 tag-event">
+            <h5>#bozar</h5>
+            <h5>#expo</h5>
+        </div><!--tag event-->
+        <div class="col-6 date-event">
+            <h5>20/06/2020</h5>
+        </div><!--date event-->
+    </div><!--description event-->  
+
+    <svg width="450" height="470" class="rect1">
+          <rect class="rect-1" width="450" height="470" style="fill:rgba(0, 0, 0, 0); stroke-width:3;stroke:rgb(255, 255, 255)" />
+    </svg>
+    <div class="lien-event">
+        <h5><a href="https://www.bozar.be/">https://www.bozar.be/</a></h5>
+    </div><!--lien event-->
+    
+    <svg width="400" height="410" class="rect2">
+        <rect class="rect-2" width="400" height="410" style="fill:rgba(0, 0, 0, 0); stroke-width:3;stroke:rgb(255, 255, 255)" />
+    </svg>
+    </div><!--show creation-->
+
+    <div class="col-12 col-lg-7 creations">
+      <div class="row recherche">
+        <div class="col filtre-random"><a>Randomize</a><input type="image" id="random" alt="random" src="img/picto-randomize.png"></div>
+        <div class="col filtre cat">date</div>
+        <div class="col filtre cat">récents</div>
+        <div class="col filtre cat">catégories</div>
+        <div class="col filtre cat">#</div>
+      </div><!--row-->
+
+      <div class="row">
+          <div class="container">
+              <div class="row  travaux">
+                <div class="col">
+                  1
+                </div>
+                <div class="col-2">
+                  2
+                </div>
+                <div class="col">
+                  3
+                </div>
+              </div><!--row-->
+              <div class="row  travaux">
+                <div class="col-3">
+                  1
+                </div>
+                <div class="col">
+                  2
+                </div>
+                <div class="col-3">
+                  3
+                </div>
+              </div><!--row-->
+              <div class="row  travaux">
+                <div class="col-5">
+                  1
+                </div>
+                <div class="col">
+                  2
+                </div>
+                <div class="col-2">
+                  3
+                </div>
+              </div><!--row-->
+              <div class="row  travaux">
+                <div class="col-3">
+                  1
+                </div>
+                <div class="col-3">
+                  2
+                </div>
+                <div class="col">
+                  3
+                </div>
+              </div><!--row-->
+            </div><!--container  travaux-->
+      </div><!--row-->
+
+      <div class="row d-flex flex-column pictogrammes">
+        <div class="col-2">
+          <img src="img/picto-plus.png" alt="ajouter">
+        </div>
+      </div><!--row-->
+
+    </div><!--creations-->
+
+  </div><!--row-->
+</div><!--portfolio etudiant-->
+
+
+<?php require("footer.php") ?>
