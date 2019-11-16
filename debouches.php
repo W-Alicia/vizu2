f514e2f6569e765aa3d7d32c9e8fe76f2d0e13c4
Alicia Wiot <xsabaku@gmail.com>
2019-11-17 00:08:22 +0100

    debouche fini

--- debouches.php
+++ debouches.php
@@ -58,7 +58,7 @@
       </h5>
     </div>
 
-    <div id="collapseFour" class="collapse show" aria-labelledby="headingFour" data-parent="#accordion">
+    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
       <div class="card-body">
       CECI EST UN DÉBOUCHÉ CECI EST UN DÉBOUCHÉ CECI EST UN DÉBOUCHÉ
       </div>
@@ -75,7 +75,7 @@
       </h5>
     </div>
 
-    <div id="collapseFive" class="collapse show" aria-labelledby="headingFive" data-parent="#accordion">
+    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
       <div class="card-body">
       CECI EST UN DÉBOUCHÉ CECI EST UN DÉBOUCHÉ CECI EST UN DÉBOUCHÉ
       </div>
@@ -87,12 +87,12 @@
     <div class="card-header" id="headingSix">
       <h5 class="mb-0"><i class="fas fa-chevron-down"></i>
         <button class="btn btn-link" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-        DÉBOUCHÉ 1
+        DÉBOUCHÉ 6
         </button>
       </h5>
     </div>
 
-    <div id="collapseSix" class="collapse show" aria-labelledby="headingSix" data-parent="#accordion">
+    <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
       <div class="card-body">
       CECI EST UN DÉBOUCHÉ CECI EST UN DÉBOUCHÉ CECI EST UN DÉBOUCHÉ
       </div>
@@ -105,43 +105,43 @@
 
 <div id="accordion" class="accordion2">
   <div class="card">
-    <div class="card-header" id="headingFour">
+    <div class="card-header" id="headingSeven">
       <h5 class="mb-0"><i class="fas fa-chevron-down"></i>
-        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
-          DÉBOUCHÉ 4
+        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+          DÉBOUCHÉ 7
         </button>
       </h5>
     </div>
 
-    <div id="collapseFour" class="collapse show" aria-labelledby="headingFour" data-parent="#accordion">
+    <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
       <div class="card-body">
       CECI EST UN DÉBOUCHÉ CECI EST UN DÉBOUCHÉ CECI EST UN DÉBOUCHÉ
       </div>
     </div>
   </div>
   <div class="card">
-    <div class="card-header" id="headingFive">
+    <div class="card-header" id="headingEight">
       <h5 class="mb-0"><i class="fas fa-chevron-down"></i>
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-        DÉBOUCHÉ 5
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+        DÉBOUCHÉ 8
         </button>
       </h5>
     </div>
-    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+    <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordion">
       <div class="card-body">
         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
       </div>
     </div>
   </div>
   <div class="card">
-    <div class="card-header" id="headingSix">
+    <div class="card-header" id="headingNine">
       <h5 class="mb-0"><i class="fas fa-chevron-down"></i>
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-        DÉBOUCHÉ 6
+        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+        DÉBOUCHÉ 9
         </button>
       </h5>
     </div>
-    <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+    <div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordion">
       <div class="card-body">
         Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
       </div>
