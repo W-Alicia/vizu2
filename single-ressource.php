7ccb72e0278b9f2579e37cf81d5b1aa7e58c2418
Alicia Wiot <xsabaku@gmail.com>
2019-11-17 20:22:13 +0100

    css ok

--- single-ressource.php
+++ single-ressource.php
@@ -1,18 +1,23 @@
 <?php require("header.php") ?>
+<img src="img/arrow.svg" alt="arrow" class="arrow2" width="75px">
 <div class="container gauche">
-<img src="img/arrow.svg" alt="arrow" class="arrow" width="75px">
-<div class="gallery">
-    <img src="img/un.jpg" alt="un" width="450px" height="450px">
-    <h4>Professeur</h4>
-</div>
-</div>
 
+<div class="gallery1"><svg width="500" height="510" class="rect3">
+                <rect class="rect-2" width="500" height="510" style="fill:rgba(0, 0, 0, 0); stroke-width:3;stroke:rgb(255, 255, 255)"></rect>
+            </svg>
+    <img src="img/ress.png" alt="un" width="450px" height="450px">
+    <h4>Professeur</h4><h4>#2D</h4>
+</div>
+</div>
 <div class="container droite">
 <h2>TITRE
 <h3>2019</h3></h2>
-<h4>Auteur</h4>
+<h4>EL GRAND CHAMACO</h4>
+<h1>HTTPS://GRANDCHAMACO.COM</h1>
+
 <p class="textdesc">
 Lorem ipsum dolor sit amet consectetur adipisicing elit.
 Itaque sed architecto aperiam deserunt id, blanditiis alias nihil voluptatem, neque velit soluta rerum, placeat aliquam nostrum dolore aut molestiae natus. Numquam.</p>
+</div>
 </div>
 <?php require("footer.php") ?>
