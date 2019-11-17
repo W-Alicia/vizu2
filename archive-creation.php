f96dfa7322cbe7608c604caba8e3c5e8f9437806
Alicia Wiot <xsabaku@gmail.com>
2019-11-17 21:44:03 +0100

    gallerie

--- archive-creation.php
+++ archive-creation.php
@@ -21,26 +21,32 @@
                
             <div class="gallerycontainer">
 
-<a class="thumbnail" href="#thumb"><img src="img/un.jpg" width="100px" height="66px" border="0" /><span><img src="img/un.jpg" /><br />Simply beautiful.</span></a>
+<a class="thumbnail" href="#thumb"><img src="img/un.jpg" width="150px" height="150px" border="0" /><span><img src="img/un.jpg" /><br />Simply beautiful.</span></a>
 
-<a class="thumbnail" href="#thumb"><img src="img/deux.jpg" width="100px" height="66px" border="0" /><span><img src="img/deux.jpg" /><br />So real, it's unreal. Or is it?</span></a>
+<a class="thumbnail" href="#thumb"><img src="img/deux.jpg" width="150px" height="150px" border="0" /><span><img src="img/deux.jpg" /><br />So real, it's unreal. Or is it?</span></a>
+
+
+
+<a class="thumbnail" href="#thumb"><img src="img/trois.jpg" width="150px" height="150px" border="0" /><span><img src="img/trois.jpg" /><br />Sushi for dinner anyone?</span></a> 
+
+<a class="thumbnail" href="#thumb"><img src="img/quatre.jpg" width="150px" height="150px" border="0" /><span><img src="img/quatre.jpg" /><br />Run wild with horses.</span></a>
 
 <br />
 
-<a class="thumbnail" href="#thumb"><img src="img/trois.jpg" width="100px" height="75px" border="0" /><span><img src="img/trois.jpg" /><br />Sushi for dinner anyone?</span></a> 
+<a class="thumbnail" href="#thumb"><img src="img/cinq.jpg" width="150px" height="150px" border="0" /><span><img src="img/cinq.jpg" /><br />Run wild with horses.</span></a>
 
-<a class="thumbnail" href="#thumb"><img src="img/quatre.jpg" width="100px" height="70px" border="0" /><span><img src="img/quatre.jpg" /><br />Run wild with horses.</span></a>
+<a class="thumbnail" href="#thumb"><img src="img/six.jpg" width="150px" height="150px" border="0" /><span><img src="img/six.jpg" /><br />Sushi for dinner anyone?</span></a> 
 
+<a class="thumbnail" href="#thumb"><img src="img/sept.jpg" width="150px" height="150px" border="0" /><span><img src="img/sept.jpg" /><br />Run wild with horses.</span></a>
+<a class="thumbnail" href="#thumb"><img src="img/huit.jpg" width="150px" height="150px" border="0" /><span><img src="img/huit.jpg" /><br />Sushi for dinner anyone?</span></a> 
 <br />
+<a class="thumbnail" href="#thumb"><img src="img/neuf.jpg" width="150px" height="150px" border="0" /><span><img src="img/neuf.jpg" /><br />Run wild with horses.</span></a>
 
-<a class="thumbnail" href="#thumb"><img src="img/quatre.jpg" width="100px" height="75px" border="0" /><span><img src="img/quatre.jpg" /><br />Sushi for dinner anyone?</span></a> 
+<a class="thumbnail" href="#thumb"><img src="img/dix.jpg" width="150px" height="150px" border="0" /><span><img src="img/dix.jpg" /><br />Sushi for dinner anyone?</span></a> 
 
-<a class="thumbnail" href="#thumb"><img src="img/cinq.jpg" width="100px" height="70px" border="0" /><span><img src="img/cinq.jpg" /><br />Run wild with horses.</span></a>
+<a class="thumbnail" href="#thumb"><img src="img/onze.jpg" width="150px" height="150px" border="0" /><span><img src="img/onze.jpg" /><br />Run wild with horses.</span></a>
+<a class="thumbnail" href="#thumb"><img src="img/douze.jpg" width="150px" height="150px" border="0" /><span><img src="img/douze.jpg" /><br />Sushi for dinner anyone?</span></a> 
 
-
-<a class="thumbnail" href="#thumb"><img src="img/six.jpg" width="100px" height="75px" border="0" /><span><img src="img/six.jpg" /><br />Sushi for dinner anyone?</span></a> 
-
-<a class="thumbnail" href="#thumb"><img src="img/sept.jpg" width="100px" height="70px" border="0" /><span><img src="img/sept.jpg" /><br />Run wild with horses.</span></a>
 
 </div>
 
