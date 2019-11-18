cdb798200bcf6dd87823bfcad40424f32846f3fe
Alicia Wiot <xsabaku@gmail.com>
2019-11-18 21:06:05 +0100

    responsive single creation, ressource

--- single-creation.php
+++ single-creation.php
@@ -1,14 +1,17 @@
 <?php require("header.php") ?>
+
 <a href="add-projet.php"><img src="img/arrow.svg" alt="arrow" class="arrow2" width="75px"></a>
 
-<div class="container gauche">
+<div class="container page-single-creation">
+<div class="row">
+<div class="col-12 col-lg-6">
 
-<div class="gallery">
     <img src="img/un.jpg" alt="un" width="450px" height="450px">
     <h4>Professeur</h4>
 </div>
-</div>
-<div class="container droite">
+
+
+<div class="col-12 col-lg-6">
 <h2>TITRE
 <h3>2019</h3></h2>
 <h4>TATIANA OLIVIER</h4>
@@ -19,6 +22,10 @@ Lorem ipsum dolor sit amet consectetur adipisicing elit.
 Itaque sed architecto aperiam deserunt id, blanditiis alias nihil voluptatem, neque velit soluta rerum, placeat aliquam nostrum dolore aut molestiae natus. Numquam.</p>
 </div>
 </div>
+</div>
+
+
+
 
 
 <?php require("footer.php") ?>
