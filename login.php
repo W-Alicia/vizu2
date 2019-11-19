0f17b07e54641fd0babd3ff51bae00f1530ab3d6
Elzbieta Pogrebniak <ela.pogrebniak@gmail.com>
2019-11-19 09:50:58 +0100

    modif responsive

--- login.php
+++ login.php
@@ -2,13 +2,13 @@
 
 <div class="container login-page">
     <div class="row">
-        <div class="col visiteur">
+        <div class="col-12 col-lg visiteur">
             <h3 class="text-center text-uppercase">VISITEUR</h3>
             <div class="etu-picto-white">
                 <a href="home.php"><img src="img/picto-logo-color.svg" alt="pictogramme"></a>
             </div>
         </div>
-        <div class="col perso">
+        <div class="col-12 col-lg perso">
             <h3 class="text-center text-uppercase">compte perso</h3>
             <div class="etu-picto">
                 <img src="img/picto-logo.svg" alt="pictogramme">
@@ -29,8 +29,9 @@
                 <div class="text-center"><button type="submit" class="btn btn-go ">GO</button></div>
             </div>
         </div>
-        <div class="col ajouter">
-            <a href="#"><img src="img/picto-plus.png" alt="ajouter"></a>
+        <div class="col-12 col-lg ajouter">
+            <a href="add-user.php"><h6 class="text-center text-uppercase">créér un compte</h6></a>
+            <a href="add-user.php"><img src="img/picto-plus.png" alt="ajouter"></a>
         </div>
     </div>
 </div>
