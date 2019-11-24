113b889f63a1db7bf263809857f70b2265146323
Kanny Gusenga <kgml94@hotmail.com>
2019-11-24 16:58:20 +0100

    mobile version kanny

--- archive-section.php
+++ archive-section.php
@@ -3,19 +3,19 @@
 <div id="sections" class="container-fluid">
     <div class="row d-flex justify-content-around ">
 
-        <div class="col-2 element d-flex justify-content align-self-center">
+        <div class="col-12 col-md-2 element d-flex justify-content align-self-center">
             <h1 class="vertical-text text-center">3D - Vidéo</h1>
             <a href="single-section.php">                
                 <div class="icon pip1"></div>
             </a>
         </div>
-        <div class="col-2 element d-flex justify-content align-self-center">
+        <div class="col-12 col-md-2 element d-flex justify-content align-self-center">
             <h1 class="vertical-text text-center">édition</h1>
             <a href="single-section.php">
                 <div class="icon pip2"></div>
             </a>
         </div>
-        <div class="col-2 element d-flex justify-content align-self-center">
+        <div class="col-12 col-md-2 element d-flex justify-content align-self-center">
             <h1 class="vertical-text text-center">Web</h1>
             <a href="single-section.php">
                 <div class="icon pip3"></div>
