deb5a729e17a97107480fce7f93fe528a0d5d7d4
Alicia Wiot <xsabaku@gmail.com>
2019-12-14 21:22:13 +0100

    svg sgl-cre ok

--- single-creation.php
+++ single-creation.php
@@ -5,19 +5,32 @@
 <div class="container page-single-creation">
 <div class="row">
 <div class="col-12 col-lg-6">
-<svg width="500" height="730" class="rectcrea">
-                <rect class="rectcrea" width="500" height="730" style="fill:rgba(0, 0, 0, 0); stroke-width:3;stroke:rgb(255, 255, 255)"></rect>
+<svg width="500" height="715" class="rectcrea">
+                <rect class="rectcrea" width="500" height="715" style="fill:rgba(0, 0, 0, 0); stroke-width:1;stroke:rgb(255, 255, 255)"></rect>
+</svg><!--svg-->
+
+<svg width="500" height="663" class="rectcrea2" style="position: absolute; left: 15px;
+        top: -54px;">
+                <rect class="rectcrea2" width="452" height="663" style="fill:rgba(0, 0, 0, 0); stroke-width:1;stroke:rgb(255, 255, 255)"></rect>
+</svg><!--svg-->
+<svg width="530" height="750" class="rectcrea3" style="    position: absolute;
+    left: -24px;
+    top: -98px;">
+                <rect class="rectcrea3" width="530" height="750" style="fill:rgba(0, 0, 0, 0); stroke-width:1;stroke:rgb(255, 255, 255)"></rect>
 </svg><!--svg-->
     <img src="img/un.jpg" alt="un" width="450px" height="550px">
-    <h4>Professeur</h4>
+    <h4 class="prof">Professeur</h4>
 </div>
 
 
 <div class="col-12 col-lg-6">
-<h2>TITRE
-<h3>2019</h3></h2>
-<h4>TATIANA OLIVIER</h4>
-<h1>@TATIANO</h1>
+<h2 class="leftres">TITRE</h2>
+<span style="color: white;
+    font-family: 'Poppins', sans-serif;
+    font-size: 2em;
+    margin-left: 4%;">2019</span>
+<h3 class="leftres">TATIANA OLIVIER</h3>
+<a href="#"><h4 class="leftres">@TATIANO</h4></a>
 
 <p class="textdesc">
 Lorem ipsum dolor sit amet consectetur adipisicing elit.
