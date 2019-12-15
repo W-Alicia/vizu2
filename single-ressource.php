52b722f79561732fb0d1421d12d951a69cb66396
Alicia Wiot <xsabaku@gmail.com>
2019-12-15 22:41:48 +0100

    responsive sg-ress

--- single-ressource.php
+++ single-ressource.php
@@ -5,14 +5,15 @@
 <div class="container single-ressource">
 <div class="row">
 <div class="col-12 col-lg-6 ressource">
-<svg width="430" height="430" class="rect4">
-                <rect class="rect-2" width="430" height="430" style="fill:rgba(0, 0, 0, 0); stroke-width:3;stroke:rgb(255, 255, 255)"></rect>
+<svg width="330" height="330" class="rect4">
+                <rect class="rect-2" width="330" height="330" style="fill:rgba(0, 0, 0, 0); stroke-width:3;stroke:rgb(255, 255, 255)"></rect>
 </svg><!--svg-->
 <div class="front-img">
     <div class="row">
-    <img src="img/ress.png" alt="un" class="img1" width="450px" height="450px" style="z-index: 1;" ></div></div>
+    <img src="img/ress.png" alt="un" class="imgone" width="320px" height="350px" style="z-index: 1;" ></div></div>
     <h4>Professeur</h4><h4>#2D</h4>
 </div><!--col-->
+
 
 
 <div class="col-12 col-lg-6 text-bottom">
